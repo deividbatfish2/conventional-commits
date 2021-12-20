15c160d43b301130341483db7b971321887d3fd6
Deivid Teixeira <deivid.teixeira@stone.com.br>
2021-12-20 12:31:43 -0300

    feat: ensure Transaction create a transaction with a list of Installments

--- src/GatewayPagamentos.Application/Model/Transaction.cs
+++ src/GatewayPagamentos.Application/Model/Transaction.cs
@@ -1,7 +1,19 @@
+using System.Collections.Generic;
+
 namespace GatewayPagamentos.Application.Model
 {
     public class Transaction
     {
         public TransactionStatus Status { get; set; }
+        public List<int> Installments { get; set; }
+
+        public Transaction()
+        {
+
+        }
+        public Transaction(int numberOfInstallments)
+        {
+            Installments = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 };
+        }
     }
 }
