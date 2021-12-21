95bb1b0e53d6138d120ab4d9396c9d05e03149f6
Deivid de Assis Teixeira <deividbatfish2@gmail.com>
2021-12-21 12:07:18 -0300

    Feat: add transaction (#19)
    * feat: ensure Transaction create a transaction with status OPEN
    
    * test: ensure Transaction create a transaction with status OPEN
    
    * feat: ensure Transaction create a transaction with a list of Installments
    
    * test: ensure Transaction create a transaction with a list of Installments
    
    * feat: ensure Transaction create all installments with status WaitForPayment
    
    * test: ensure Transaction create all installments with status WaitForPayment
    
    * feat: ensure Transaction create all installments with status WaitForPayment
    
    * test: ensure Transaction create all installments with status WaitForPayment

---
+++ src/GatewayPagamentos.Application/Model/Transaction.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+namespace GatewayPagamentos.Application.Model
+{
+    public class Transaction
+    {
+        public TransactionStatus Status { get; set; }
+        public List<Installment> Installments { get; set; }
+
+        public Transaction()
+        {
+
+        }
+        public Transaction(int numberOfInstallments)
+        {
+            Installments = CreateInstallments(numberOfInstallments, InstallmentStatus.WaitForPayment);
+        }
+
+        private List<Installment> CreateInstallments(int numberOfInstallments, InstallmentStatus installmentStatus)
+        {
+            var installments = new List<Installment>();
+            for (int i = 0; i < numberOfInstallments; i++)
+            {
+                installments.Add(new Installment
+                {
+                    Status = installmentStatus
+                });
+            }
+            return installments;
+        }
+    }
+}
