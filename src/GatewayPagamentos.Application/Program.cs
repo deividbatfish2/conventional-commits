a99efc33d000abab70a5daa8b1210e6820b355b4
Deivid Teixeira <deivid.teixeira@stone.com.br>
2021-12-22 10:02:38 -0300

    fix: change method of logs

--- src/GatewayPagamentos.Application/Program.cs
+++ src/GatewayPagamentos.Application/Program.cs
@@ -6,7 +6,7 @@ namespace GatewayPagamentos.Application
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Hello World!");
         }
     }
 }
